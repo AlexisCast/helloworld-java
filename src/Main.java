d254eb27e66abf613a8cca3e72bccf5a73b802d2
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-06-29 23:15:20 -0600

    add Scanner for keyboard input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Type something in the keyboard: ");
+
+        Scanner keyboardInput = new Scanner(System.in);
+        String variable = keyboardInput.nextLine();
+
+        System.out.println("Your variable: " + variable);
     }
 }

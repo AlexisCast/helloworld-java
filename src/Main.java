91228c946d83902afb8fa719d9df73dca17aa754
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-06-30 18:05:25 -0600

    fix: clean Main

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        //  50: Needs
-        //  30: Things we want
-        //  20: savings
-
-        Scanner keyboardInput = new Scanner(System.in);
-
-        System.out.println("Enter your salary:");
-
-        float salary = keyboardInput.nextFloat();
-
-        float needs = salary * .5f;
-
-        float otherThings = salary * .3f;
-
-        float savings = salary * .2f;
-
-        System.out.println("Structure your salary in the following way from $" + salary + ":");
-        System.out.println("Needs: " + needs);
-        System.out.println("Things we want: " + otherThings);
-        System.out.println("Savings: " + savings);
-
+        System.out.println("Hello world!");
     }
 }
